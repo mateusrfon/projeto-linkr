0272137d2792a73ed39e34c095d16c6683204611
Mateus Ribeiro <mateusrfon@gmail.com>
2021-05-25 16:51:29 -0300

    Initial App

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import UserContext from '../contexts/UserContext';
+
+export default function App() {
+    const [userInfo, setUserInfo] = useState({});
+
+    return (
+        <UserContext.Provider value={userInfo}>
+            <GlobalStyle/>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact></Route>
+                    <Route path="/sign-up"></Route>
+                    <Route path="/timeline"></Route>
+                    <Route path="/my-posts"></Route>
+                    <Route path="/my-likes"></Route>
+                    <Route path="/user/:id"></Route>
+                    <Route path="/hashtag/:hashtag"></Route>
+                </Switch>
+            </BrowserRouter>
+        </UserContext.Provider>
+    );
+}
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+        box-sizing: border-box;
+    }
+`;
