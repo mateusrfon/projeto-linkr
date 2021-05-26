9cf4fcc85aeba2a4d88c4cb6825207df531f7ac0
Mateus Ribeiro <mateusrfon@gmail.com>
2021-05-26 16:53:37 -0300

    feature-publish

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,43 @@ import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 import UserContext from '../contexts/UserContext';
+import Navbar from './Navbar/Navbar';
 import Timeline from './timeline/Timeline';
 
 export default function App() {
-    const [userInfo, setUserInfo] = useState({});
+    const [userInfo, setUserInfo] = useState({
+        "token": '',
+        "user": {
+            "id": '',
+            "email": '',
+            "username": '',
+            "avatar": ''
+        }
+    });
 
     return (
-        <UserContext.Provider value={userInfo}>
-            <GlobalStyle />
+        <UserContext.Provider value={{ userInfo, setUserInfo }}>
+            <GlobalStyle/>
             <BrowserRouter>
                 <Switch>
                     <Route path="/" exact></Route>
                     <Route path="/sign-up"></Route>
                     <Route path="/timeline">
+                        <Navbar/>
                         <Timeline />
                     </Route>
-                    <Route path="/my-posts"></Route>
-                    <Route path="/my-likes"></Route>
-                    <Route path="/user/:id"></Route>
-                    <Route path="/hashtag/:hashtag"></Route>
+                    <Route path="/my-posts">
+                        <Navbar/>
+                    </Route>
+                    <Route path="/my-likes">
+                        <Navbar/>
+                    </Route>
+                    <Route path="/user/:id">
+                        <Navbar/>
+                    </Route>
+                    <Route path="/hashtag/:hashtag">
+                        <Navbar/>
+                    </Route>
                 </Switch>
             </BrowserRouter>
         </UserContext.Provider>
@@ -34,7 +52,9 @@ const GlobalStyle = createGlobalStyle`
         border: 0;
         font-size: 100%;
         font: inherit;
+        color: inherit;
         vertical-align: baseline;
         box-sizing: border-box;
+        text-decoration: none;
     }
 `;
