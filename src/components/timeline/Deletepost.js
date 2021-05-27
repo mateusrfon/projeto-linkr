4d195af7b221c306f2f64ccaee75182e00884025
Lucas <lucas_tavares@id.uff.br>
2021-05-27 15:45:51 -0300

    workin on delete post

--- src/components/timeline/Deletepost.js
+++ src/components/timeline/Deletepost.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useContext } from "react"
 import UserContext from "../../contexts/UserContext"
-import { confirmAlert } from 'react-confirm-alert';
+
 
 export default function DeletePost({post}){
     const {userInfo} = useContext(UserContext)
