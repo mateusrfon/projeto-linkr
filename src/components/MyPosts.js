a811796919b387f8ea237580cacccbfe3cb5c3d5
Mateus Ribeiro <mateusrfon@gmail.com>
2021-05-27 15:56:08 -0300

    local login implemented

---
+++ src/components/MyPosts.js
@@ -0,0 +1,50 @@
+import Main from './Main';
+import axios from 'axios';
+import { useContext, useEffect, useState, useCallback } from 'react';
+import UserContext from '../contexts/UserContext';
+
+export default function MyPosts() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const { userInfo } = useContext(UserContext);
+
+    const handleGetPosts = useCallback(
+        (isFirstTime) => {
+            if (isFirstTime) {
+                setIsLoading(true);
+            }
+            const promise = axios.get(
+                'https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts',
+                {
+                    headers: {
+                        Authorization: `Bearer ${userInfo.token}`,
+                    },
+                }
+            );
+            promise.then((response) => {
+                setIsLoading(false);
+                setData(response.data.posts);
+            });
+
+            promise.catch((error) => {
+                setIsLoading(false);
+                alert('Houve um erro por favor recarregue a pagina');
+            });
+        },
+        [userInfo.token]
+    );
+
+    useEffect(() => {
+        handleGetPosts(true);
+    }, [handleGetPosts]);
+
+    return (
+        <Main
+            posts={data}
+            setPosts={setData}
+            title="my posts"
+            loading={isLoading}
+            getPosts={handleGetPosts}
+        />
+    );
+}
