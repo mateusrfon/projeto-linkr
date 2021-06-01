51045dc0f0675c97ae0f180bafe81c6e179f6a58
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-01 15:45:43 -0300

    link box

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -62,7 +62,7 @@ const StyledNavbar = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    z-index: 9999;
+    z-index: 900;
     @media (max-width: 1000px) {
         padding: 0 14px 0 17px;
     }
